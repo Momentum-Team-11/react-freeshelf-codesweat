c2ae83457ccebbeec0567e1a341694d2424ad4b9
KarenElaine <karenelaine.stewart@yahoo.com>
2022-03-27 11:00:21 -0400

    added 'null' information for when no publisher or date is available

--- freeshelf/src/BookCard.js
+++ freeshelf/src/BookCard.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 
+// todo for today
+// need to add null images and information for when there is nothing provide
+// need to have more information change to less information
+// need to change button to filled in carrot arrow
+
 const BookCard = (props) => {
     const {book} = props
     const [isExpanded, setIsExpanded] = useState(false)
@@ -9,10 +14,9 @@ const BookCard = (props) => {
         <p>{book.author}</p>
         <p>{book.shortDescription}</p>
         <img src={book.coverImageUrl} alt={book.shortDescription}></img>
-        <button
-        onClick={() => {
-            console.log("button clicked");
+        <button onClick={() => { 
             setIsExpanded(!isExpanded);
+            console.log("button clicked!")
         }}
         >
         More Information
@@ -20,10 +24,11 @@ const BookCard = (props) => {
         {isExpanded && (
         <>
             <p>URL: {book.url}</p>
-            <p>Publisher: {book.publisher}</p>
-            <p>Publication Date: {book.publicationDate}</p>
+            <p>Publisher: {book.publisher || "Information Not Available"}</p>
+            <p>Publication Date: {book.publicationDate || "Information Not Available"}</p>
             <p>Full description: {book.detailedDescription}</p>
         </>
+        
         )}
     </>
     );
